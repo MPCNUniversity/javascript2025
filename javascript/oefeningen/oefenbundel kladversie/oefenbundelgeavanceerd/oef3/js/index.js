126b0a262f8db907d43b615cdf26bd0cba0b73d3
Friedel <mpcnetwork.manager@outlook.com>
2025-09-29 09:03:03 +0200

    maandag update

--- javascript/oefeningen/oefenbundel kladversie/oefenbundelgeavanceerd/oef3/js/index.js
+++ javascript/oefeningen/oefenbundel kladversie/oefenbundelgeavanceerd/oef3/js/index.js
@@ -1,2 +1,10 @@
 // Print getallen die een bepaald patroon volgen : Print de getallenreeks 1, 2, 4, 8, 16, 32, ...,
 // totdat de waarde groter is dan een door de gebruiker opgegeven getal.
+let a = parseInt(window.prompt("Geef een getal op tussen 100 en 1000"));
+let getallenreeks = 1;
+
+for (let b =1;getallenreeks<=a;b++){
+    console.log(getallenreeks);
+    getallenreeks = getallenreeks+getallenreeks;
+}
+console.log(getallenreeks);

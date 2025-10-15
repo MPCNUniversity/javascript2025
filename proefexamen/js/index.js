2f25f371c35228a380b5d52e5804d6a652bf9bdb
Friedel <mpcnetwork.manager@outlook.com>
2025-10-15 12:19:48 +0200

    store procedures

---
+++ proefexamen/js/index.js
@@ -0,0 +1,40 @@
+// // Welkom op de site van Friedel Majoor : Manager at MPCNetwork
+// // Met let (in functie{},voorkeur) en var (globaal) waarden geven aan namen (vb naam leeftijd)
+//
+// let naam = "Friedel";
+// console.log("Welkom op de site van " + naam);
+//
+// var leeftijd = 42;
+// // let is "moderner" en aanbevolen , kan worden aangepast
+// let score = 100;
+// score = 120;
+//
+// const PI = 3.14;
+// // selectie : if, else , else if | De else is je standaardwaarde als er geen oplossing is -> loop invoer vrager
+// let temperatuur = 25;
+//
+// if (temperatuur > 30) {
+//     console.log("Het is heet!");
+// } else if {
+//     console.log("Lekker weer!");
+// } else {
+//     console.log("frisjes");
+// }
+
+// let elementen = [46,15,7,55,19,18];
+// for (let element of elementen){
+//     if (element%2==0){
+//         console.log(element)
+//     }
+// }
+
+let isEven = true;
+let getal =  21 ; //kan ander getal zijn
+
+if (getal%2===0){
+    isEven = true;
+} else {
+    isEven = false;
+}
+console.log(isEven)
+
